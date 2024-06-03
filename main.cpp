0552e0b6955d4ff147f36f142ba1b3b23aed6d42
sreekel <50909565+sreeesjlqkjfkqjsdflkqjsflqjsdflqjf@users.noreply.github.com>
2024-06-03 13:52:53 +0200

    rajout touche q pour quitter

--- main.cpp
+++ main.cpp
@@ -2,41 +2,52 @@
 #include "SteeringBehavior.h"
 #include <cmath>
 
-
 int main() {
-    sf::RenderWindow window{sf::VideoMode{800, 800}, "Game"};
-    window.setFramerateLimit(30);
-    Flock f{100, 800, 800};
-    f.max_speed = 200.f;
-    f.max_force = 400.f;
-    f.put_on_rectangle(800, 800, 10, 10);
-    Seek seek{f, {600, 400}};
+  sf::RenderWindow window{sf::VideoMode{800, 800}, "Game"};
+  window.setFramerateLimit(30);
+  Flock f{100, 800, 800};
+  f.max_speed = 200.f;
+  f.max_force = 400.f;
+  f.put_on_rectangle(800, 800, 10, 10);
+  Seek seek{f, {600, 400}};
 
-    bool running{true};
-    sf::Clock c{};
-    auto date = c.getElapsedTime();
-    while (running) {
-        sf::Event event{};
-        while (window.pollEvent(event)) {
-            switch (event.type) {
-                case sf::Event::EventType::Closed:
-                    running = false;
-                    break;
-                default:
-                    break;
-            }
+  bool running{true};
+  sf::Clock c{};
+  auto date = c.getElapsedTime();
+  while (running) {
+    sf::Event event{};
+    while (window.pollEvent(event)) {
+      switch (event.type) {
+
+      case sf::Event::KeyPressed: {
+        switch (event.key.code) {
+
+        case sf::Keyboard::Q: {
+          running = false;
+          break;
         }
-        window.clear(sf::Color::White);
-
-        auto tmp = c.getElapsedTime();
-        seek.compute();
-        f.update(tmp - date);
-        date = tmp;
-        f.draw(window);
-        window.display();
+        default:
+          break;
+        }
+      }
+      case sf::Event::EventType::Closed:
+        running = false;
+        break;
+      default:
+        break;
+      }
     }
+    window.clear(sf::Color::White);
 
-    window.close();
+    auto tmp = c.getElapsedTime();
+    seek.compute();
+    f.update(tmp - date);
+    date = tmp;
+    f.draw(window);
+    window.display();
+  }
 
-    return 0;
+  window.close();
+
+  return 0;
 }
