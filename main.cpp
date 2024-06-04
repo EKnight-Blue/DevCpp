bfb93ebe5557889371fa3e3de676abdfa413eca4
sreekel <50909565+sreeesjlqkjfkqjsdflkqjsflqjsdflqjf@users.noreply.github.com>
2024-06-04 22:36:41 +0200

    ajout de qlq idiomes et démo de imgui

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 #include "Flock.h"
 #include "Steering.h"
 #include "imgui-SFML.h"
+#include "imgui.h"
+#include <SFML/System/Clock.hpp>
 #include <cmath>
 int main() {
     sf::RenderWindow window{sf::VideoMode{800, 800}, "Game"};
     window.setFramerateLimit(30);
+    ImGui::SFML::Init(window);
     Flock f1{100, 800, 800};
     f1.max_speed = 200.f;
     f1.max_force = 800.f;
@@ -33,19 +36,19 @@ int main() {
     Steering steer{Steering::Behavior::Arrival,
                    {.arrival = {.target = {400., 400.}, .range = 200.}},
                    20.};
-    bool running{true};
     sf::Clock c{};
-    auto date = c.getElapsedTime();
-    while (running) {
+    auto dt = c.restart();
+    while (window.isOpen()) {
         sf::Event event{};
         while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
             switch (event.type) {
 
             case sf::Event::KeyPressed:
                 switch (event.key.code) {
 
                 case sf::Keyboard::Q:
-                    running = false;
+                    window.close();
                     break;
 
                 default:
@@ -54,22 +57,24 @@ int main() {
                 break;
 
             case sf::Event::EventType::Closed:
-                running = false;
+                window.close();
                 break;
             default:
                 break;
             }
         }
         window.clear(sf::Color::White);
-
+        dt = c.restart();
+        ImGui::SFML::Update(window, dt);
+        ImGui::ShowDemoWindow();
         auto tmp = c.getElapsedTime();
         cb.compute(f1);
         cb.compute(f2);
-        f1.update(tmp - date);
-        f2.update(tmp - date);
-        date = tmp;
+        f1.update(dt);
+        f2.update(dt);
         f1.draw(window);
         f2.draw(window);
+        ImGui::SFML::Render(window);
         window.display();
     }
 }
