04d4a069c5022780273bdac3a9b0c42cf919daff
Loic Chevalier <loicchechevalier@gmail.com>
2024-06-18 20:55:46 +0200

    Calm Sonarlint on main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "Behavior/AtomicBehavior.h"
-#include "Behavior/CombinedBehavior.h"
 #include "Creatures/Flock.h"
 #include "World/FiniteWorld.h"
 #include "interface.h"
@@ -7,10 +6,72 @@
 #include <cmath>
 #include <iostream>
 
-long long int fps_somme = 0;
-long long int fps_compte = 0;
+
+void handle_events(sf::RenderWindow & window) {
+    static sf::Vector2f last_mouse_position;
+    static int last_button{-1};
+    static sf::View vue = window.getDefaultView();
+
+    sf::Vector2f new_mouse_position;
+
+    sf::Event event{};
+    while (window.pollEvent(event)) {
+        ImGui::SFML::ProcessEvent(event);
+        switch (event.type) {
+            case sf::Event::MouseButtonPressed:
+                if (last_button != -1)
+                    break;
+                last_button = event.mouseButton.button;
+                last_mouse_position = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+                break;
+            case sf::Event::MouseButtonReleased:
+                if (event.mouseButton.button == last_button)
+                    last_button = -1;
+                break;
+            case sf::Event::MouseMoved:
+                if (last_button != sf::Mouse::Middle)
+                    break;
+                new_mouse_position = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+                vue.move(last_mouse_position - new_mouse_position);
+                window.setView(vue);
+                last_mouse_position = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+                break;
+            case sf::Event::MouseWheelScrolled: {
+                const sf::Vector2f pos_ini_vue = vue.getCenter();
+                vue.setCenter(
+                        window.mapPixelToCoords(sf::Mouse::getPosition(window)));
+                vue.zoom(1.f + 0.1f * event.mouseWheelScroll.delta);
+                vue.setCenter(pos_ini_vue);
+                window.setView(vue);
+                break;
+            }
+
+            case sf::Event::Resized: {
+                sf::FloatRect zoneVisible(
+                        0, 0, static_cast<float>(event.size.width),
+                        static_cast<float>(event.size.height));
+                vue = sf::View(zoneVisible);
+                window.setView(vue);
+                break;
+            }
+            case sf::Event::KeyPressed:
+                if (event.key.code == sf::Keyboard::Q) {
+                    window.close();
+                }
+                break;
+
+            case sf::Event::EventType::Closed:
+                window.close();
+                break;
+            default:
+                break;
+        }
+    }
+}
 
 int main() {
+    float fps_somme = 0;
+    long long int fps_compte = 0;
 #ifdef WIN32
     std::system("color");
 #endif
@@ -18,9 +79,7 @@ int main() {
     texture.loadFromFile("./resources/bg.jpg");
     sf::Sprite background(texture);
     sf::RenderWindow window{sf::VideoMode{800, 800}, "Game"};
-    sf::View vue = window.getDefaultView();
     window.setFramerateLimit(0);
-    // Ihm test{&window};
     FiniteWorld w{1600.f, 1600.f};
     w.flocks.emplace_back(Animal::Bird, 20.f, 2000, 50.f, 100.f);
     w.behaviors.emplace_back();
@@ -43,107 +102,12 @@ int main() {
 
     sf::Clock c{};
     auto dt = c.restart();
-    //-_-_-_-__-_-_-_-_-_-_-__-_-_-_-Etats souris
-    bool selection{false};
-    bool translation{false};
-    sf::Vector2f position_précédente_souris_monde;
-    //-_-_-_-_-_-_-_-_-_-_-_-_-_-_
     std::cout << "\x1B[31m";
 
     ImGui::SFML::Init(window);
 
     while (window.isOpen()) {
-        sf::Event event{};
-        while (window.pollEvent(event)) {
-            ImGui::SFML::ProcessEvent(event);
-            switch (event.type) {
-            case sf::Event::MouseButtonPressed:
-                switch (event.mouseButton.button) {
-                case sf::Mouse::Left:
-                    // selection
-                    if (!translation && !selection) {
-                        selection = true;
-                    }
-                    break;
-                case sf::Mouse::Middle:
-                    // translation
-                    if (!translation && !selection) {
-                        position_précédente_souris_monde =
-                            window.mapPixelToCoords(
-                                sf::Mouse::getPosition(window));
-                        translation = true;
-                    }
-                    break;
-                default:
-                    break;
-                }
-                break;
-            case sf::Event::MouseButtonReleased:
-                switch (event.mouseButton.button) {
-                case sf::Mouse::Left:
-                    // selection
-                    if (!translation && selection) {
-                        selection = false;
-                    }
-                    break;
-                case sf::Mouse::Middle:
-                    // fin de la translation
-                    if (translation && !selection) {
-                        translation = false;
-                    }
-                    break;
-                default:
-                    break;
-                }
-            case sf::Event::MouseMoved:
-
-                if (translation) {
-                    const sf::Vector2f position_nouvelle_souris_monde =
-                        window.mapPixelToCoords(sf::Mouse::getPosition(window));
-                    vue.move(-position_nouvelle_souris_monde +
-                             position_précédente_souris_monde);
-                    window.setView(vue);
-                    position_précédente_souris_monde =
-                        window.mapPixelToCoords(sf::Mouse::getPosition(window));
-                }
-                break;
-            case sf::Event::MouseWheelScrolled: {
-                const sf::Vector2f pos_ini_vue = vue.getCenter();
-                vue.setCenter(
-                    window.mapPixelToCoords(sf::Mouse::getPosition(window)));
-                vue.zoom(1.f + 0.1f * event.mouseWheelScroll.delta);
-                vue.setCenter(pos_ini_vue);
-                window.setView(vue);
-                break;
-            }
-
-            case sf::Event::Resized: {
-                sf::FloatRect zoneVisible(
-                    0, 0, static_cast<float>(event.size.width),
-                    static_cast<float>(event.size.height));
-                vue = sf::View(zoneVisible);
-                window.setView(vue);
-                break;
-            }
-            case sf::Event::KeyPressed:
-                switch (event.key.code) {
-
-                case sf::Keyboard::Q:
-                    window.close();
-                    break;
-
-                default:
-                    break;
-                }
-                break;
-
-            case sf::Event::EventType::Closed:
-                window.close();
-                break;
-            default:
-                break;
-            }
-        }
+        handle_events(window);
         window.clear(sf::Color::White);
         dt = c.restart();
 
@@ -152,8 +116,6 @@ int main() {
         //-_-_-_-_-__-_-_-_-_-_-_-_--_-_DESSSIN
         window.draw(background);
         w.draw(window);
-        // test.affichage_ihm(&w);
-
         ImGui::SFML::Update(window, dt);
         ImGui::ShowDemoWindow();
         if (ImGui::Begin("Menu")) {
@@ -163,13 +125,13 @@ int main() {
         ImGui::SFML::Render(window);
         window.display();
 
-        float fps = 1E6 / dt.asMicroseconds();
+        float fps = 1E6f / static_cast<float>(dt.asMicroseconds());
         if (fps < 90.f){
             fps_somme += fps;
             fps_compte++;
         }
         std::cout << "\x1B[2K\r" << fps << std::flush;
     }
-    std::cout << "\n moyenne : " << fps_somme / fps_compte << "fps\x1B[0m"
+    std::cout << "\n moyenne : " << fps_somme / static_cast<float>(fps_compte) << "fps\x1B[0m"
               << std::flush;
 }
