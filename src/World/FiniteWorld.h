89906de1343c00e79fdbc5e9a0e8a927b832d228
Loic Chevalier <loicchechevalier@gmail.com>
2024-06-19 00:38:42 +0200

    Calmed SonarLint

--- src/World/FiniteWorld.h
+++ src/World/FiniteWorld.h
@@ -4,7 +4,7 @@
 
 class FiniteWorld : public World{
 public:
-    FiniteWorld(float width, float height) : width{width}, height{height}, tree{{0.f, 0.f}, {width, height}, nullptr} {};
+    FiniteWorld(float width, float height) : tree{{0.f, 0.f}, {width, height}, nullptr}, width{width}, height{height} {};
     [[nodiscard]] sf::Vector2f position_difference(sf::Vector2f const& v1, sf::Vector2f const & v2) const override;
     void update(sf::Time delta_time) override;
     [[nodiscard]] std::unique_ptr<NeighborIterator> make_neighbor_iterator(Animal animal, FlockMember const &member, float range, float cos_fov) const override;
